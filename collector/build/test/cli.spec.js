95771f57a8dec26586654436e9c1f1e2c78f5b86
Eric Heikes <eheikes@gmail.com>
2018-02-04 18:42:06 -0800

    chore(build): Rebuild the test files

--- collector/build/test/cli.spec.js
+++ collector/build/test/cli.spec.js
@@ -1,18 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const path = require("path");
-const child_process_1 = require("child_process");
+const fs_1 = require("fs");
+const path_1 = require("path");
+const util_1 = require("util");
 describe('CLI', () => {
-    const bin = path.resolve(__dirname, '../build/src/main.js');
+    const bin = path_1.resolve(__dirname, '../build/src/main.js');
     it('script should be executable', () => {
-        return new Promise((resolve, reject) => {
-            const proc = child_process_1.spawn(bin);
-            proc.on('close', code => {
-                if (code !== 0) {
-                    return reject(new Error(`Binary exited with error code ${code}`));
-                }
-                resolve();
-            });
-        });
+        // Note that Windows doesn't support executable flags;
+        //   it will only check if the file is visible.
+        return util_1.promisify(fs_1.access)(bin, fs_1.constants.X_OK);
     });
 });
--- collector/build/test/main.spec.js
+++ collector/build/test/main.spec.js
@@ -4,6 +4,6 @@ const main_1 = require("../src/main");
 describe('main', () => {
     it('should parse the CLI arguments', () => {
         expect(main_1.opts).toEqual(jasmine.any(Object));
-        expect(main_1.opts.$0).toMatch(/jasmine-ts$/);
+        expect(main_1.opts.$0).toMatch(/jasmine-ts/);
     });
 });
