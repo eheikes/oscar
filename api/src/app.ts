c9fe66fa98bb15f51e073cf7483394b12011d054
Eric Heikes <eheikes@gmail.com>
2019-04-07 21:12:51 -0700

    feat(api): Enable CORS

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,3 +1,4 @@
+import * as cors from 'cors'
 import * as express from 'express'
 import { resolve }  from 'path'
 import { getConfig } from './lib/config'
@@ -11,6 +12,13 @@ const controllers: {[key: string]: express.RequestHandler} = {
 const config = getConfig(resolve(__dirname, '..'))
 const app = express()
 
+// Enable CORS on all (including pre-flight) requests.
+app.options('*', cors({
+  credentials: true,
+  methods: ['DELETE', 'GET', 'OPTIONS', 'POST', 'PUT']
+}))
+
+// Set up the configured routes.
 config.routes.forEach(route => {
   app[route.method](route.path, controllers[route.controller])
 })
--- api/test/integration/app.spec.ts
+++ api/test/integration/app.spec.ts
@@ -10,12 +10,27 @@ const fillInPath = (path: string) => {
 }
 
 describe('app', () => {
-  getConfig(resolve(__dirname, '..', '..')).routes.filter(isGet).forEach(route => {
+  const routes = getConfig(resolve(__dirname, '..', '..')).routes.filter(isGet)
+
+  routes.forEach(route => {
     it(`should respond to GET ${route.path}`, () => {
       return request(app)
         .get(fillInPath(route.path))
         .expect(200)
         .expect('Content-Type', /json/)
     })
+  })
+
+  it('should return CORS headers', () => {
+    // Make an OPTIONS request so Access-Control-Allow-Methods is included.
+    return request(app)
+      .options(fillInPath(routes[0].path))
+      .expect('Access-Control-Allow-Origin', /./)
+      .expect('Access-Control-Allow-Credentials', 'true')
+      .expect('Access-Control-Allow-Methods', /GET/)
+      .expect('Access-Control-Allow-Methods', /POST/)
+      .expect('Access-Control-Allow-Methods', /PUT/)
+      .expect('Access-Control-Allow-Methods', /DELETE/)
+      .expect('Access-Control-Allow-Methods', /OPTIONS/)
   })
 })
