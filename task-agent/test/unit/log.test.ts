a3ad7c01a367cbc0c67d1a02f9412fd53918dc8c
Eric Heikes <eheikes@gmail.com>
2020-02-21 22:38:31 -0800

    test(log): Add tests for log routine

---
+++ task-agent/test/unit/log.test.ts
@@ -0,0 +1,34 @@
+import { log } from '../../src/log'
+
+describe('log()', () => {
+  let spy: jest.SpyInstance
+
+  beforeEach(() => {
+    spy = jest.spyOn(console, 'log')
+    spy.mockImplementation(() => {})
+  })
+
+  afterEach(() => {
+    spy.mockRestore()
+  })
+
+  it('should prefix with the app and namespace', () => {
+    log('namespace', 'message')
+    expect(spy.mock.calls[0][0]).toBe('\u001b[34moscar:task-agent:namespace\u001b[39m')
+  })
+
+  it('should stringify objects', () => {
+    log('namespace', { foo: 'bar' })
+    expect(spy.mock.calls[0][1]).toBe('\u001b[37m {"foo":"bar"}\u001b[39m')
+  })
+
+  it('should cast other values to strings', () => {
+    log('namespace', 'message', 5)
+    expect(spy.mock.calls[0][1]).toBe('\u001b[37m message 5\u001b[39m')
+  })
+
+  it('should separate values by a space', () => {
+    log('namespace', 'foo', 'bar', 'baz')
+    expect(spy.mock.calls[0][1]).toBe('\u001b[37m foo bar baz\u001b[39m')
+  })
+})
