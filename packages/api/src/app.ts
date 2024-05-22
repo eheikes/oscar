fccdb093b49855bfc26a4ff7dba7da4be0941d8e
Eric Heikes <eheikes@gmail.com>
2024-05-22 00:38:25 -0700

    feat(server): Add server option

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -1,10 +1,7 @@
 import express from 'express'
-import serverless from 'serverless-http'
 
-const app = express()
+export const app = express()
 
 app.get('/', (_req, res) => {
   res.json({ message: 'Hello from Express on AWS Lambda!' })
 })
-
-export const handler = serverless(app)
