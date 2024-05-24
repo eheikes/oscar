f72286877863f60c90f8cf318766ea0ca82f1503
Eric Heikes <eheikes@gmail.com>
2024-05-24 00:38:52 -0700

    feat(app): Disable x-powered-by header

---
+++ packages/api/test/e2e/app.spec.ts
@@ -0,0 +1,10 @@
+import request from 'supertest'
+import { app } from '../../src/app.js'
+
+describe('app', () => {
+  it('should not include an x-powered-by header', async () => {
+    const response = await request(app)
+      .get(`/`)
+    expect(response.headers['x-powered-by']).toBeUndefined()
+  })
+})
--- packages/api/test/unit/app.spec.ts
+++ packages/api/test/unit/app.spec.ts
@@ -4,12 +4,15 @@ import { app as originalApp } from '../../src/app.js'
 describe('app', () => {
   let app: typeof originalApp
   let expressGetSpy: jasmine.Spy
+  let expressSetSpy: jasmine.Spy
   let mockExpress: any
 
   beforeEach(async () => {
     expressGetSpy = jasmine.createSpy('express.get')
+    expressSetSpy = jasmine.createSpy('express.set')
     mockExpress = {
-      get: expressGetSpy
+      get: expressGetSpy,
+      set: expressSetSpy
     }
     ;({ app } = await esmock('../../src/app.js', {
       express: () => mockExpress
@@ -18,6 +21,10 @@ describe('app', () => {
 
   it('should export an Express app', () => {
     expect(app).toBe(mockExpress)
+  })
+
+  it('should turn off the X-Powered-By header', () => {
+    expect(expressSetSpy).toHaveBeenCalledWith('x-powered-by', false)
   })
 
   it('should create the routes', () => {
