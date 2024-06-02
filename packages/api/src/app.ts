7eab72bb28c17960850114d3104e67befd11eda4
Eric Heikes <eheikes@gmail.com>
2024-06-02 15:37:35 -0700

    refactor(app): Switch to true async middleware

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -6,15 +6,9 @@ export const app = express()
 
 app.set('x-powered-by', false)
 
-app.get('/items', (req, res, next) => {
-  void (async () => {
-    try {
-      const result = await getItems(req.query)
-      res.json(result)
-    } catch (err) {
-      next(err)
-    }
-  })()
+app.get('/items', async (req, res, _next) => {
+  const result = await getItems(req.query)
+  res.json(result)
 })
 
 app.use(errorHandler)
