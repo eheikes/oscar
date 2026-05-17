cb1ede1ad709ad8a7ed22f80a3b1c3bd12a3ad83
Eric Heikes <eheikes@gmail.com>
2026-05-17 15:47:57 -0700

    feat(migrations): Add default types

---
+++ packages/api/src/migrations/0003-types-data.ts
@@ -0,0 +1,18 @@
+/* c8 ignore start */
+import knex from 'knex'
+
+const upSql = `INSERT INTO types (id, readable) VALUES
+('listen', 'listen to'),
+('listen-passive', 'passively listen to'),
+('news', 'catch up on'),
+('play', NULL),
+('read', NULL),
+('task', 'do'),
+('watch', NULL),
+('watch-passive', 'passively watch');
+`
+
+const downSql = 'DELETE * FROM types;'
+
+export const up = (knex: knex.Knex): knex.Knex.SchemaBuilder => knex.schema.raw(upSql)
+export const down = (knex: knex.Knex): knex.Knex.SchemaBuilder => knex.schema.raw(downSql)
